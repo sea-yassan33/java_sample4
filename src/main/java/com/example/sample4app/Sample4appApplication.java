2dd1083e641aa718c84992bab6cf1b6d40751697
Yasuda <a.yasuda_tr51@me.com>
2023-06-11 16:05:05 +0900

    first_commit

---
+++ src/main/java/com/example/sample4app/Sample4appApplication.java
@@ -0,0 +1,13 @@
+package com.example.sample4app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sample4appApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sample4appApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sample4app/Sample4appApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sample4app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sample4appApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
