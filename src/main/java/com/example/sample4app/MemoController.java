baee4f3f633a5eff30ba8b16247129348d8116a2
Yasuda <a.yasuda_tr51@me.com>
2023-06-11 20:24:51 +0900

    data_save1

--- src/main/java/com/example/sample4app/MemoController.java
+++ src/main/java/com/example/sample4app/MemoController.java
@@ -1,11 +1,18 @@
 package com.example.sample4app;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.sample4app.repositories.PersonRepository;
+
+import jakarta.transaction.Transactional;
 
 @Controller
 public class MemoController {
@@ -14,12 +21,19 @@ public class MemoController {
   PersonRepository repository;
 
   @RequestMapping("/")
-  public ModelAndView index(ModelAndView mav){
+  public ModelAndView index(@ModelAttribute("formModel") Person Person,ModelAndView mav){
     mav.setViewName("index");
     mav.addObject("title", "Hello page");
     mav.addObject("msg","This is Sample4-SITE");
     Iterable<Person> list =repository.findAll();
     mav.addObject("data", list);
     return mav;
+  }
+
+  @RequestMapping(value = "/", method = RequestMethod.POST)
+  @Transactional
+  public ModelAndView form(@ModelAttribute("formModel") Person Person,ModelAndView mav){
+    repository.saveAndFlush(Person);
+    return new ModelAndView("redirect:/");
   }
 }
