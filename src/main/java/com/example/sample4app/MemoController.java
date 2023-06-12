ab2004909462226ff53c15feefe3b8881b7119f7
Yasuda <a.yasuda_tr51@me.com>
2023-06-12 15:35:08 +0900

    Edit_function1

--- src/main/java/com/example/sample4app/MemoController.java
+++ src/main/java/com/example/sample4app/MemoController.java
@@ -1,11 +1,13 @@
 package com.example.sample4app;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,6 +35,22 @@ public class MemoController {
   @RequestMapping(value = "/", method = RequestMethod.POST)
   @Transactional
   public ModelAndView form(@ModelAttribute("formModel") Person Person,ModelAndView mav){
+    repository.saveAndFlush(Person);
+    return new ModelAndView("redirect:/");
+  }
+
+  @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+  public ModelAndView edit(@ModelAttribute Person Person, @PathVariable int id, ModelAndView mav){
+    mav.setViewName("edit");
+    mav.addObject("title", "edit Person.");
+    Optional<Person> data = repository.findById((long)id);
+    mav.addObject("formModel", data.get());
+    return mav;
+  }
+
+  @RequestMapping(value = "/edit", method = RequestMethod.POST)
+  @Transactional
+  public ModelAndView update(@ModelAttribute Person Person, ModelAndView mav){
     repository.saveAndFlush(Person);
     return new ModelAndView("redirect:/");
   }
--- src/main/java/com/example/sample4app/repositories/PersonRepository.java
+++ src/main/java/com/example/sample4app/repositories/PersonRepository.java
@@ -1,5 +1,7 @@
 package com.example.sample4app.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.example.sample4app.Person;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long>{
+  public Optional<Person> findById(Long name);
 }
